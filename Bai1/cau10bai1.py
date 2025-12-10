0bc9e297fc6e102b5457e66cdb817fde5ddfd7ca
nguyencuong1209as-droid <nguyencuong1209as@gmail.com>
2025-12-10 22:18:29 +0700

    Add files via upload

---
+++ Bai1/cau10bai1.py
@@ -0,0 +1,11 @@
+print(" Sinh vien : Nguyen Van Cuong")
+print(" Ma so SV: 245751030110048")
+print("##############################")
+
+
+
+a='hi i am python programmer'
+b=a.split()
+print(b)
+c=' '.join(b)
+print(c)
