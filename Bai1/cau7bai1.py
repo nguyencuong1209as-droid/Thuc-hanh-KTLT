5101eb5ddff6d89e7fd595a2eca7e7db37e3a19c
nguyencuong1209as-droid <nguyencuong1209as@gmail.com>
2025-12-11 10:22:11 +0700

    Update cau7bai1.py

--- Bai1/cau7bai1.py
+++ Bai1/cau7bai1.py
@@ -1,6 +1,11 @@
-print('7)')
+print(" Sinh vien : Nguyen Van Cuong")
+print(" Ma so SV: 245751030110048")
+print("##############################")
+
+
 n=int(input("Nhap mot so: "))
 d={}
 for i in range(1,n+1):
     d[i]=i*i
 print(d)
+
