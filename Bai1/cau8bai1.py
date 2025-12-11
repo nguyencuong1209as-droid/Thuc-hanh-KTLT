82b04cfacf7edc5b5a83fb4084bd1f1df1685ec0
nguyencuong1209as-droid <nguyencuong1209as@gmail.com>
2025-12-11 10:27:34 +0700

    Update cau8bai1.py

--- Bai1/cau8bai1.py
+++ Bai1/cau8bai1.py
@@ -1,4 +1,8 @@
-print('8)')
+print(" Sinh vien : Nguyen Van Cuong")
+print(" Ma so SV: 245751030110048")
+print("##############################")
+
+
 a,b=1,2
 total=0
 print(a,end=" ")
@@ -8,3 +12,4 @@ while a <= 4000000:
     print(a,end=" ")
     a,b=b,a+b
 print("1nTong cac so chan trong day Finonacoi:", total)
+
