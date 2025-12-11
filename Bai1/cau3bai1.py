1c52a4087be4f37f2f9a49bc7fb9f77f5dbd08fa
nguyencuong1209as-droid <nguyencuong1209as@gmail.com>
2025-12-11 10:15:54 +0700

    Add files via upload

---
+++ Bai1/cau3bai1.py
@@ -0,0 +1,10 @@
+print(" Sinh vien : Nguyen Van Cuong")
+print(" Ma so SV: 245751030110048")
+print("##############################")
+
+
+n=int(input("Enter a number--->"))
+if n % 2 ==0:
+    print ("EVEN Number");
+else:
+    print ( "ODD Number");
