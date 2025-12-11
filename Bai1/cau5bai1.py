cb5bafe17cc0e19ce2e842f4faeb0df82775fd3c
nguyencuong1209as-droid <nguyencuong1209as@gmail.com>
2025-12-11 10:17:52 +0700

    Update cau5bai1.py

--- Bai1/cau5bai1.py
+++ Bai1/cau5bai1.py
@@ -1,5 +1,10 @@
-print('5)')
+print(" Sinh vien : Nguyen Duc Long")
+print(" Ma so SV: 245751030110072")
+print("##############################")
+
+
 n=int(input("Enter a number: "))
 while n>=0:
     print(n)
     n -=1
+
