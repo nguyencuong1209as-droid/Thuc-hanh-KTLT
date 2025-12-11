aff702c3197000575e840185e0a84f433b10a74f
nguyencuong1209as-droid <nguyencuong1209as@gmail.com>
2025-12-11 10:02:39 +0700

    Update cau4bai1.py

--- Bai1/cau4bai1.py
+++ Bai1/cau4bai1.py
@@ -1,4 +1,8 @@
-print('4)')
+print(" Sinh vien : Nguyen Van Cuong")
+print(" Ma so SV: 245751030110048")
+print("##############################")
+
+
 i=1;
 a=int(input("Enter a: "))
 b=int(input("Enter b: "))
@@ -6,3 +10,4 @@ for j in range (a,b+1):
     print("i:",i,"j:",j)
     print(i,"/",j)
     print(i/j);
+
