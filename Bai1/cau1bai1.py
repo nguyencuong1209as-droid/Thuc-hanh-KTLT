2b78413e0c33d85962c8daffc43a46b50ba98e97
nguyencuong1209as-droid <nguyencuong1209as@gmail.com>
2025-12-11 09:10:54 +0700

    Update cau1bai1.py

--- Bai1/cau1bai1.py
+++ Bai1/cau1bai1.py
@@ -1,5 +1,11 @@
+print(" Sinh vien : Nguyen Van Cuong")
+print(" Ma so SV: 245751030110048")
+print("##############################")
+
+
 print('1)')
 n1=float(input("Enter n1 value:"))
 n2=float(input("Enter n2 value:"))
 if n1>n2:
     print("n1 is big")
+
