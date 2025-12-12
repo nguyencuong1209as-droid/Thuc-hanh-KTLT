9dc0a246551b1a32152e45c229769a8890e8f2d4
nguyencuong1209as-droid <nguyencuong1209as@gmail.com>
2025-12-12 22:38:00 +0700

    Add files via upload

---
+++ Bai5/5-2.py
@@ -0,0 +1,16 @@
+print(" Sinh vien : Nguyen Van Cuong")
+print(" Ma so SV: 245751030110048")
+print("##############################")
+
+
+import turtle, random
+colosrs=["red", "green", "blue", "orange", "purple", "pink", "yellow"]
+painter = turtle.Turtle()
+painter. pensize (3)
+for i in range (100):
+  color = random.choice(colors)
+  painter-pencolor (color)
+  painter.circle (100)
+  painter.right (30)
+  painter.left (60)
+  painter.setposition (0, 0)
